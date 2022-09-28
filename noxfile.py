c6045e7191e17b3f7a88639b0656273cc0f02617
Levi Blaney <shadycuz@gmail.com>
2022-09-28 10:54:00 +0000

    dont use constaints

--- noxfile.py
+++ noxfile.py
@@ -13,19 +13,20 @@ def install_with_constraints(session, *args, **kwargs):
         session.run(
             "poetry",
             "export",
-            "--dev",
+            "--only",
+            "dev",
             "--format=requirements.txt",
             "--without-hashes",
             f"--output={requirements.name}",
             external=True,
         )
-        session.install(f"--constraint={requirements.name}", *args, **kwargs)
+        session.install("-r", requirements.name, *args, **kwargs)
 
 
 @nox.session(python="3.9")
 def coverage(session):
     """Upload coverage data."""
-    install_with_constraints(session, "coverage[toml]", "codecov")
+    install_with_constraints(session)
     session.run("coverage", "xml", "--fail-under=0")
     session.run("codecov", *session.posargs)
 
@@ -35,7 +36,7 @@ def tests(session):
     args = session.posargs or ["--cov", "-m", "not e2e"]
     session.run("poetry", "install", "--no-dev", external=True)
     install_with_constraints(
-        session, "coverage[toml]", "pytest", "pytest-cov", "pytest-mock"
+        session,
     )
     session.run("pytest", *args)
 
@@ -43,21 +44,19 @@ def tests(session):
 @nox.session(python=["3.9", "3.8"])
 def lint(session):
     args = session.posargs or locations
-    install_with_constraints(
-        session, "flake8", "flake8-black", "flake8-bugbear", "flake8-import-order"
-    )
+    install_with_constraints(session)
     session.run("flake8", *args)
 
 
 @nox.session(python=["3.9", "3.8"])
 def mypy(session):
     args = session.posargs or locations
-    install_with_constraints(session, "mypy")
+    install_with_constraints(session)
     session.run("mypy", *args)
 
 
 @nox.session(python="3.9")
 def black(session):
     args = session.posargs or locations
-    install_with_constraints(session, "black")
+    install_with_constraints(session)
     session.run("black", *args)
