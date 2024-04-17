b46353676a1b3ddefe53527c69c51f86e4b3d2aa
David Hutchison <dhutchison86+github@googlemail.com>
2024-04-17 13:38:57 +0100

    Feat/initial hooks support (#315)
    * feat(compliance): wip ramblings
    
    * feat(hooks): continued development of hooks
    
    * feat(hooks): added more tests and code docs for template hooks
    
    * feat(hooks): documentation

---
+++ examples/unit/hooks/resources/test_hooks_resources.py
@@ -0,0 +1,112 @@
+from pathlib import Path
+
+import pytest
+
+from cloud_radar.cf.unit import ResourceHookContext, Template
+
+
+@pytest.fixture()
+def configure_hooks():
+    # Add in locally defined hooks
+    Template.Hooks.resources = {
+        "AWS::S3::Bucket": [my_s3_naming_hook, my_s3_encryption_hook]
+    }
+
+    yield
+
+    # Clear the hooks after
+    Template.Hooks.resources = {}
+
+
+# Example hook that verifies that the rendered bucket name (after
+# substitutions) includes the region the template is being deployed to.
+def my_s3_naming_hook(context: ResourceHookContext) -> None:
+    name: str = context.resource_definition.get_property_value("BucketName")
+
+    if context.template.Region not in name:
+        raise ValueError(
+            (
+                f"{context.logical_id} - All buckets are "
+                "expected to include the region in their name"
+            )
+        )
+
+
+# Example hook that verifies  that all S3 bucket definitions
+# have the "BucketEncryption" property set
+def my_s3_encryption_hook(context: ResourceHookContext) -> None:
+    # Use one of the built in functions to confirm the property exists
+    context.resource_definition.assert_has_property("BucketEncryption")
+
+
+# Helper method to load a template file relative to this test file
+def load_template(filename: str):
+    template_path = Path(__file__).parent / filename
+    template = Template.from_yaml(template_path)
+
+    return template
+
+
+# Test case showing that when all hooks pass, no errors are raised.
+@pytest.mark.usefixtures("configure_hooks")
+def test_basic_all_success():
+    template = load_template("naming_resources.yaml")
+
+    # In these cases I'll usually use a non-existant region to ensure a real region
+    # is not hard coded
+
+    # Render the stack, this will execute the resource level hooks
+    template.create_stack(params={"pName": "test"}, region="xx-west-3")
+
+
+# Test case showing that when a resource hook causes an error,
+# we see it at the point of creating the rendered stack
+@pytest.mark.usefixtures("configure_hooks")
+def test_basic_resource_failure():
+    template = load_template("naming_resources_no_encryption.yaml")
+
+    # Render the stack, this will execute the resource level hooks
+    with pytest.raises(
+        AssertionError, match="Resource 'rS3Bucket' has no property BucketEncryption."
+    ):
+        template.create_stack(params={"pName": "test"}, region="xx-west-3")
+
+
+# The contents of this test template are mostly the same as the one we used for
+# test_basic_resource_failure, but this time we have the appropriate Metadata
+# at the Template level to ignore the resource hook that was failing
+@pytest.mark.usefixtures("configure_hooks")
+def test_template_suppression_success():
+    template = load_template("naming_resources_no_encryption_template_suppression.yaml")
+
+    template.create_stack(params={"pName": "test"}, region="xx-west-3")
+
+
+# This test is similar to test_template_suppression_success,
+# but shows that if the hook being ignored is different from the
+# failing one that we still see the failure.
+@pytest.mark.usefixtures("configure_hooks")
+def test_template_suppression_diff_hook():
+    template = load_template(
+        "naming_resources_no_encryption_template_suppression_diff_rule.yaml"
+    )
+
+    with pytest.raises(
+        AssertionError, match="Resource 'rS3Bucket' has no property BucketEncryption."
+    ):
+        template.create_stack(params={"pName": "test"}, region="xx-west-3")
+
+
+# The contents of this test template are mostly the same as the one we used for
+# test_basic_resource_failure, but this time we have the appropriate Metadata
+# at the Resource level to ignore the resource hook that was failing
+@pytest.mark.usefixtures("configure_hooks")
+def test_resource_suppression_success():
+    template = load_template(
+        "naming_resources_no_encryption_resource_suppression_diff_rule.yaml"
+    )
+
+    with pytest.raises(
+        AssertionError, match="Resource 'rS3Bucket' has no property BucketEncryption."
+    ):
+        template.create_stack(params={"pName": "test"}, region="xx-west-3")
--- src/cloud_radar/cf/unit/__init__.py
+++ src/cloud_radar/cf/unit/__init__.py
@@ -1,4 +1,6 @@
+from ._hooks import ResourceHookContext
+from ._resource import Resource
 from ._stack import Stack
 from ._template import Template
 
-__all__ = ["Template", "Stack"]
+__all__ = ["Template", "Stack", "Resource", "ResourceHookContext"]
