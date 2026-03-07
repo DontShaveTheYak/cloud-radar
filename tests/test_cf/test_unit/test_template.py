6c31a60c8a023a20f0eaa179aade0d5ef47d66e7
Levi <shadycuz@gmail.com>
2026-03-07 11:58:44 -0500

    Speed up DevContainer creation (#421)

--- tests/test_cf/test_unit/test_template.py
+++ tests/test_cf/test_unit/test_template.py
@@ -802,3 +802,35 @@ def test_unknown_dynamic_references():
         ),
     ):
         template.render()
+
+
+def test_load_allowed_functions_no_transforms():
+    template = Template({})
+    template.load_allowed_functions()
+    assert template.allowed_functions == functions.ALL_FUNCTIONS
+
+
+def test_load_allowed_functions_single_transform():
+    template = Template({"Transform": "AWS::Serverless-2016-10-31"})
+    template.load_allowed_functions()
+    expected = {
+        **functions.ALL_FUNCTIONS,
+        **functions.TRANSFORMS["AWS::Serverless-2016-10-31"],
+    }
+    assert template.allowed_functions == expected
+
+
+def test_load_allowed_functions_multiple_transforms():
+    template = Template({"Transform": ["AWS::Serverless-2016-10-31", "AWS::Include"]})
+    template.load_allowed_functions()
+    expected = {
+        **functions.ALL_FUNCTIONS,
+        **functions.TRANSFORMS["AWS::Serverless-2016-10-31"],
+        **functions.TRANSFORMS["AWS::Include"],
+    }
+    assert template.allowed_functions == expected
+
+
+def test_load_allowed_functions_invalid_transform():
+    with pytest.raises(ValueError):
+        Template({"Transform": "InvalidTransform"})
